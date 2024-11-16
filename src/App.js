206075104d9654a21013de10d34dfd54bd88645d
Stephanie Metieh <stephaniemetieh@gmail.com>
2024-11-16 21:05:16 +0100

    Create layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import "./App.css";
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <p className="text-red-600">hi</p>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
