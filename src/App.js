8895670d580d8d86fc7799f7dfc66b8510fdf423
Stephanie Metieh <stephaniemetieh@gmail.com>
2024-11-16 18:43:56 +0100

    create react app

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return <p className="text-red-600">hi</p>;
+}
+
+export default App;
